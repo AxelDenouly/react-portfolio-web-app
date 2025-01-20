4f01f12ccc77784fb7f42415e340fa49723673b8
Axel Denouly <axeljoo2935@gmail.com>
2025-01-20 11:33:38 +0100

    Finition du formulaire pour le rendre fonctionnel et modification des parties en anglais

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,52 +8,69 @@ const Contact = () => {
 
       <div className="contact__container grid">
         <div className="contact__info">
-          <h3 className="contact__title">Let's Talk About Your Plan For Me</h3>
+          <h3 className="contact__title">Parlons de vos plans pour moi</h3>
           <p className="contact__details">
-            Don't like forms ? Send me an email 😊
+            Vous n'aimez pas les formulaires ? Envoyez-moi un email 😊
           </p>
         </div>
 
-        <form action="" className="contact__form">
+        <form
+          action="https://api.web3forms.com/submit"
+          method="POST"
+          className="contact__form"
+        >
+          <input
+            type="hidden"
+            name="access_key"
+            value="c2d7a2a7-bf3e-446f-8e7c-f8e96e7e152f"
+          />
+
           <div className="contact__form-group">
             <div className="contact__form-div">
               <input
+                name="last_name"
                 type="text"
                 className="contact__form-input"
-                placeholder="Your Name"
+                placeholder="Votre Nom"
               />
             </div>
 
             <div className="contact__form-div">
               <input
+              name="email"
                 type="email"
                 className="contact__form-input"
-                placeholder="Your Email"
+                placeholder="Votre Email"
               />
             </div>
           </div>
 
           <div className="contact__form-div">
             <input
+            name="subject"
               type="text"
               className="contact__form-input"
-              placeholder="Your Subject"
+              placeholder="Objet"
             />
           </div>
 
           <div className="contact__form-div contact__form-area">
             <textarea
-              name=""
+              name="message"
               id=""
               cols="30"
               rows="10"
               className="contact__form-input"
-              placeholder="Your message..."
+              placeholder="Rédigez votre message..."
             ></textarea>
           </div>
 
-          <button className="btn" href="#">
-            <a href="#" className="btn__send">Send Message</a>
+          <input type="hidden" name="redirect" value="https://web3forms.com/success" />
+
+          <button type="submite" className="btn">
+            <a className="btn__send">
+              Envoyer le message
+            </a>
           </button>
         </form>
       </div>
